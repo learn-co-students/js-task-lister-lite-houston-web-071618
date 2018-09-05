aa76a759de5b2250c53c28741013158ce51ef905
Christopher Arnold <chrisarnold2010@gmail.com>
2018-09-05 17:58:06 -0500

    added eventListener for taskForm

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,29 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
+  // const taskList = new TaskList();
+
+
+// - users should be able to type a task into the input field
+const taskForm = document.getElementById('main-content')
+const addTaskForm = document.getElementById('create-task-form')
+const parseJSON = resp => resp.json()
+
+taskForm.addEventListener('submit', function(event) {
+	event.preventDefault();
+debugger 
+	
+})
+
+
+function addTaskToList(task) {
+	debugger 
+}
+
+// - users should be able to click some form of a submit button
+
+
+// - the task string that the user provided should appear on the DOM after the submit button has been activated
+
+
+
 });
