c83a50b90983771f7bb9f62087e7978408f21663
farverio <github email address>
2018-09-07 09:20:51 -0500

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,30 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
+  // const taskList = new TaskList();
+  const taskForm = document.getElementById('create-task-form');
+  const taskUL = document.getElementById('tasks');
+  
+  function addTask(e) {
+    e.preventDefault()
+    let taskName = e.target.children[1].value
+    createTaskElement(taskName)
+    e.target.children[1].value = ""
+  }
+  
+  function createTaskElement(taskString) {
+    let newTaskHTML = `<li>${taskString} <button data-id="${taskString}">X</button></li>`
+    taskUL.innerHTML += newTaskHTML
+  }
+  
+  function deleteTask(e) {
+    let deleteButton = e.target.dataset.id
+    
+    if (deleteButton) {
+      let taskToDelete = e.target.parentElement
+      taskToDelete.remove()
+    }
+  }
+  
+  taskForm.addEventListener('submit', e => addTask(e))
+  taskUL.addEventListener('click', e => deleteTask(e))
 });
