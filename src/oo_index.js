1640ec251b6f0b581f0536748e43d6e8b3136a1d
pabloargueta <pabloargueta27@gmail.com>
2018-09-09 01:39:53 -0500

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,44 @@
+// let taskDiv = document.querySelector('#list')
 
 document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
+  const taskForm = document.querySelector('#create-task-form')
+
+  const ulTag = document.querySelector('#tasks')
+
+  const renderApp = () => {
+    ulTag.innerHTML = taskList.renderAllTasks()
+
+    // taskUl.innerHTML = ''
+    // taskArray = taskList.renderAllTasks()
+    // taskArray.forEach(task => {
+    //   taskUl.appendChild(task)
+    // })
+
+  };
+  // ul.innerHTML = taskList.renderAllTasks
+  taskForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const newDescription = event.target.querySelector('#new-task-description').value
+
+    if (newDescription) {
+      taskList.createNewTask(newDescription)
+      renderApp()
+    }
+  })
+
+  ulTag.addEventListener('click', (event) => {
+    // debugger
+    if (event.target.nodeName === "BUTTON") {
+      taskList.deleteTask(parseInt(event.target.dataset.taskId))
+
+      renderApp()
+    }
+  })
+
+
+
+
 });
+
+
--- src/taskList.js
+++ src/taskList.js
@@ -1,2 +1,34 @@
-class TaskList {
-}
+const TaskList = (() => {
+    let taskListId = 0;
+    return class {
+        constructor() {
+            this.tasks = []
+        }
+
+        createNewTask(description) {
+            const newTask = new Task(description)
+            this.tasks.push(newTask)
+        }
+
+        renderAllTasks() {
+            // let newUl = document.createElement('ul')
+            // let liArray = this.tasks.map(task => task.render())
+            // liArray.forEach(li => {
+            //     newUl.append(li)
+            // });
+            // // debugger
+            // return newUl
+
+            return this.tasks.map((task) => task.render()).join("");
+        }
+
+        deleteTask(taskId) {
+            this.tasks = this.tasks.filter(task => {
+                return task.id !== taskId
+            })
+        }
+    }
+
+})();
+
+
