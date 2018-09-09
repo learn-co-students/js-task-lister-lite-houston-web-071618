df036b6052bdc202c77c10e80e2a15ea8acb464c
ELehotzky <Lehotzky.Erica@gmail.com>
2018-09-08 21:54:55 -0500

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,41 @@
+// const taskList = new TaskList();
 
-document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
-});
+console.log("DOM loaded");
+
+const taskLister = document.getElementById("main-content");
+const taskListerForm = document.getElementById("create-task-form");
+
+taskLister.addEventListener("submit", addTask);
+
+function addTask(event) {
+	event.preventDefault();
+	const taskDesc = document.getElementById("new-task-description").value;
+	const taskList = document.getElementById("tasks");
+	const listItem = document.createElement("li");
+	listItem.innerHTML = `
+		${taskDesc}
+		<button id="deleteBtn">X</button>
+		`
+	if (taskDesc === "") {
+		alert("Please enter a task description!");
+	} else 
+	taskList.append(listItem);
+}
+
+taskLister.addEventListener("click", deleteTask);
+
+
+
+// const deleteBtn = document.getElementById("deleteBtn");
+
+// if (deleteBtn) {
+// 	document.deleteBtn.addEventListener("click", deleteTask);
+// }
+
+function deleteTask(event) {
+	// debugger
+	const deleteBtn = (event.target.id === "deleteBtn")
+	if (deleteBtn) {
+	event.target.parentElement.remove();
+}
+}
